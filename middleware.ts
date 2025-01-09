1e7aa64043c343436254131b440f6b9a55861d73
Zubair Imtiaz <106450046+ZubairImtiaz3@users.noreply.github.com>
2025-01-09 22:38:30 +0500

    chore: update middleware to run only on dashboard routes

--- middleware.ts
+++ middleware.ts
@@ -5,20 +5,11 @@ export async function middleware(request: NextRequest) {
     const user = await getLoggedInUser();
 
     // Restrict dashboard from unauthorized
-    if (request.nextUrl.pathname.startsWith('/dashboard') && user === null) {
+    if (user === null) {
         return NextResponse.redirect(new URL('/', request.url));
     }
 }
 
 export const config = {
-    matcher: [
-        /*
-         * Match all request paths except for the ones starting with:
-         * - _next/static (static files)
-         * - _next/image (image optimization files)
-         * - favicon.ico (favicon file)
-         * Feel free to modify this pattern to include more paths.
-         */
-        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-    ],
+    matcher: ['/dashboard/:path*'],
 }
